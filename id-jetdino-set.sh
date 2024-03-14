ec8ac6c1add0eabc03f22bb2880f3c109a9cbb82
quorecs <90849589+psmtnhljs@users.noreply.github.com>
2024-03-14 17:04:13 +0800

    Update id-jetdino-set.sh

--- id-jetdino-set.sh
+++ id-jetdino-set.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 fallocate -l 1G swapfile && chmod 600 swapfile && mkswap swapfile && swapon swapfile && swapon --show && echo '/root/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
 read -p"虚拟内存成功添加"
-sleep 10
 echo 1 > /proc/sys/vm/overcommit_memory
-read -p "Overcommit关闭"
+read -p "完成"
